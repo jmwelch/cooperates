59eade2114696db920a3a9735c40bd940a3a00bc
Neil Sethi <neilsethi@gmail.com>
2013-04-14 22:08:51 -0400

    fixed the db error

--- db/migrate/20100414_fix_db_shit.rb
+++ db/migrate/20100414_fix_db_shit.rb
@@ -6,11 +6,5 @@ class FixDbShit < ActiveRecord::Migration
   def self.down
      rename_column :inventories, :quanity_left, :quantity_left
 
-      create_table(:foods, :primary_key => 'id')  do |t|      
-        t.integer "food_id",      :null => false
-        t.integer "user_id"
-        t.string  "fname"
-        t.integer "price"
-      end
   end
 end
