31697210d4fb7d99efd00c97435d120f76eb6d0d
Neil Sethi <neilsethi@gmail.com>
2013-04-17 10:46:15 -0400

    Retroactively adding Ben's seeds.rb work

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ group :development, :test do
   gem 'sqlite3'
 end
 
+gem 'seed-fu', '~> 2.2.0'
 gem 'devise'
 gem 'best_in_place'
 gem 'ransack'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+#
+# config.action_mailer.perform_deliveries = false
+# ["one","two","three","four"].each do |fake|
+#  	User.find_or_create_by_name(fake)
+#  end
+
+#user = User.create! :name => 'John Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+# params =  { :user =>
+#     {
+#     :username =>
+#     :email => 'consu@a.com',
+#     :password => 'aaaaaa',
+#     :password_confirmation => 'aaaaaa',
+#       }
+#   }
+# }
+# User.create!(params[:user])
+# end
