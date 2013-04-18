9dac97d80b1713c59e83d4bae9ee02f30963dabd
jmwelch <jared.welch@emory.edu>
2013-04-18 01:03:09 -0400

    Modifies Inventory Controller

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -3,6 +3,7 @@ class InventoriesController < ApplicationController
 	def index
 		@user = User.find(params[:id])
 		@inventory = @user.inventory
+		@list = []
 	end
 
 	def show
@@ -17,17 +18,24 @@ class InventoriesController < ApplicationController
 	def create
 		@inventory = Inventory.new(params[:inventory])
 		@inventory.user_id = current_user.id
-		@first = Ingredient.first
-		if !@first.nil?
-			@exists = Ingredient.where("ingredient_name = ?", params[:inventory][:ingredient_name])
-			if !@exists.empty?
-				@inventory.ingredient_id = @exists.first.ingredient_id
-			else
-				@inventory.ingredient_id = Ingredient.last.id + 1
-			end
+		@exists = Ingredient.last
+		if !@exists.nil?
+			@inventory.ingredient_id = @exists.id + 1
 		else
 			@inventory.ingredient_id = 1
 		end
+
+#		@first = Ingredient.first
+#		if !@first.nil?
+#			@exists = Ingredient.where("ingredient_name = ?", params[:inventory][:ingredients_attributes]["0"][:ingredient_name])
+#			if !@exists.empty?
+#				@inventory.ingredient_id = @exists.first.id
+#			else
+#				@inventory.ingredient_id = Ingredient.last.id + 1
+#			end
+#		else
+#			@inventory.ingredient_id = 1
+#		end
 		if @inventory.save
 			redirect_to @inventory
 		else
