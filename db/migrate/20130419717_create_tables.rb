b8743372183d961f2ec224cac57aaf8d0bfa0253
Neil Sethi <neilsethi@gmail.com>
2013-04-19 19:39:49 -0400

    Created schema. Added initial migration file.

---
+++ db/migrate/20130419717_create_tables.rb
@@ -0,0 +1,66 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :user_type
+      t.string :description
+      t.string :price_range
+      t.string :rating
+      t.string :cuisine
+      t.string :address
+      t.string :email,                  :default => "", :null => false
+      t.string :encrypted_password,     :default => "", :null => false
+      t.string :reset_password_token
+      t.datetime :reset_password_sent_at
+      t.datetime :remember_created_at
+      t.integer  :sign_in_count,          :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+      
+      t.timestamps
+     end
+
+     add_index :users, :username, :unique => true
+     add_index :users, :email, :unique => true
+     add_index :users, :reset_password_token, :unique => true
+     
+     create_table :foods do |t|
+       t.string :name
+       t.integer :user_id
+       t.float :price
+       t.string :ingredient_name
+     end
+     
+     create_table :ingredients do |t|
+       t.string :ingredient_name
+       t.integer :user_id
+       t.integer :food_id
+     end
+     
+     create_table :transactions do |t|
+       t.string :ingredient_name
+       t.datetime :purchase_date
+       t.float :purchase_price
+       t.integer :quantity
+       t.integer :bought_from
+       t.integer :sold_to
+    end
+    
+    create_table :stock do |t|
+      t.string :ingredient_name
+      t.integer :quantity
+      t.integer :user_id
+    end
+    
+    create_table :friendships do |t|
+      t.integer :user_id
+      t.integer :friend_id
+      
+      t.timestamps
+     end
+     
+     
+  end
+end
