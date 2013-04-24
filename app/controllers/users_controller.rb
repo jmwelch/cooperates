2f4c5df601281af6ddd2d5761a5502dfeaa73290
Stanley Chiang <stanchiang23@gmail.com>
2013-04-23 21:40:58 -0400

    finish devise setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,56 @@
+class UsersController < ApplicationController
+  respond_to :html, :json
+  
+  before_filter :authenticate_user!#, except: [:index]
+
+  def index
+    @users = User.all
+  end
+
+  def search
+    @search =User.search(params[:q])
+    @users = @search.result
+
+ # @search.build_condition if @search.conditions.empty?
+ # @search.build_sort if @search.sorts.empty?
+  end    
+
+  def show
+    @user = User.find(params[:id])
+    
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, notice: "user was successfully created."
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+      respond_with @user
+  end
+
+  def destroy
+    @user = user.find(params[:id])
+    @user.destroy
+    redirect_to users_url
+  end
+
+  def import
+    User.import(params[:file])
+    redirect_to users_url, notice: "Products imported"
+  end
+end
---
+++ db/migrate/20130419717_create_tables.rb
@@ -0,0 +1,42 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+
+     create_table :foods do |t|
+       t.string :name
+       t.integer :user_id
+       t.float :price
+       t.string :ingredient_name
+     end
+     
+     create_table :ingredients do |t|
+       t.string :ingredient_name
+       t.integer :user_id
+       t.integer :food_id
+       t.integer :quantity_used
+     end
+     
+     create_table :transactions do |t|
+       t.string :ingredient_name
+       t.datetime :purchase_date
+       t.float :purchase_price
+       t.integer :quantity
+       t.integer :bought_from
+       t.integer :sold_to
+    end
+    
+    create_table :stock do |t|
+      t.string :ingredient_name
+      t.integer :quantity
+      t.integer :user_id
+    end
+    
+    create_table :friendships do |t|
+      t.integer :user_id
+      t.integer :friend_id
+      
+      t.timestamps
+     end
+     
+     
+  end
+end
