b65f7574a5526f5f976c51595d661a5145a5ef5d
Neil Sethi <neilsethi@gmail.com>
2013-04-25 12:47:35 -0400

    Added welcome view. Changed routes.rb to route to welcome by default (did not change anything else about routes EXCEPT that). Edited/fixed application.html.erb.

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,9 @@ Cooperates::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'users#index'
-
+   #root :to => 'users#index'
+   root :to => 'welcome#index'
+   
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
