0c371f7dbd8cf6778598c2b27b4665a1dc9db163
Neil Sethi <neilsethi@gmail.com>
2013-04-25 12:49:19 -0400

    Removed the routes change, don't want to mess with routes just yet.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,6 @@ Cooperates::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    #root :to => 'users#index'
-   root :to => 'welcome#index'
    
   # See how all your routes lay out with "rake routes"
 
