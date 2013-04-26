65e5aa304643db2ddccc210223923fd47f04125b
jmwelch <jared.welch@emory.edu>
2013-04-26 11:00:43 -0400

    Allows adding of Ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,4 +4,23 @@ class IngredientsController < ApplicationController
 		@user = User.find(params[:id])
 		@ingredients = @user.ingredients
 	end
+
+	def show
+		@ingredient = Ingredient.find(params[:id])
+	end
+
+	def new
+		@ingredient = Ingredient.new
+	end
+
+	def create
+		@ingredient = Ingredient.new(params[:ingredient])
+		@ingredient.user_id = current_user.id
+
+		if @ingredient.save
+			redirect_to @ingredient
+		else
+			render 'new'
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Cooperates::Application.routes.draw do
   # Keep in mind you can assign values other than :controller and :action
 	match 'users/search' => 'users#search'
 	match 'users/:id/food/new' => 'foods#new', :as => 'new_food'
-	match 'users/:id/inventory/new' => 'inventories#new', :as => 'new_inventory'
+	match 'users/:id/ingredient/new' => 'ingredients#new', :as => 'new_ingredient'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
