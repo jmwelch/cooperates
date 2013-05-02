10ce4a2ebcfbccb5da4207272cdf1f3152d6b1fa
Stanley Chiang <stanchiang23@gmail.com>
2013-05-02 02:28:32 -0400

    searching with ransack and geocoder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  
+  before_filter :vars
+def vars
+  @allusers = User.find(:all)
+  @allingredients = Ingredient.find(:all)
+  @allfoods = Food.find(:all)
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,8 +19,26 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @food = @user.foods
     @ingredients = @user.ingredients.select(:ingredient_name).uniq.sort_by{|u| u.ingredient_name}
+    @a=[]
+    @b=[]
+    @restOfIngred = @allingredients - @user.ingredients
+    @user.ingredients.each do |myingredient|
+      @restOfIngred.each do |youringredient|
+        if youringredient.ingredient_name == myingredient.ingredient_name
+              @a << youringredient
+        end
+      end
+    end
+    
+    @a.each do |restaurant|
+      @allusers.each do |user|
+        if restaurant.user_id == user.id
+          @b << user
+        end
+      end
+    end
+    @c =@b.uniq
 
-    @ingred_connect = User.search(:ingredients_ingredient_name_cont => "flour", username_not_cont => current_user.username).result(:distinct => true)
   end
 
   def new
@@ -29,34 +47,34 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-		if current_user.id != params[:id].to_i
-			redirect_to user_path(params[:id]), :notice => "You cannot edit #{@user.username}'s profile!"
-		end
-  end
+    if current_user.id != params[:id].to_i
+     redirect_to user_path(params[:id]), :notice => "You cannot edit #{@user.username}'s profile!"
+   end
+ end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to @user, notice: "user was successfully created."
-    else
-      render "new"
-    end
+ def create
+  @user = User.new(params[:user])
+  if @user.save
+    redirect_to @user, notice: "user was successfully created."
+  else
+    render "new"
   end
+end
 
-  def update
-    @user = User.find(params[:id])
-    @user.update_attributes(params[:user])
-    respond_with @user
-  end
+def update
+  @user = User.find(params[:id])
+  @user.update_attributes(params[:user])
+  respond_with @user
+end
 
-  def destroy
-    @user = user.find(params[:id])
-    @user.destroy
-    redirect_to users_url
-  end
+def destroy
+  @user = user.find(params[:id])
+  @user.destroy
+  redirect_to users_url
+end
 
-  def import
-    User.import(params[:file])
-    redirect_to users_url, notice: "Products imported"
-  end
+def import
+  User.import(params[:file])
+  redirect_to users_url, notice: "Products imported"
+end
 end
